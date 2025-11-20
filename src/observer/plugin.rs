b218b3933c0b42a9a5799f8daa9431d6a70f28ef
MorningDuringNight <129632808+MorningDuringNight@users.noreply.github.com>
2025-11-20 16:28:59 -0500

    Upstream observation grabbing some of the AI for training (#40)
    * feat(policy): receive observation info
    
    * feat: write q table
    
    * feat: create and update qtable
    
    * feat: update qtable
    
    * feat: set reward
    
    * feat: policy write random or learned action
    
    * feat: controller receive action
    
    * feat: can train
    
    * feat: update in every 6 frame
    
    * feat: add debug, update wall reward
    
    * feat: update q table
    
    * feat(policy): update circle
    
    ---------
    
    Co-authored-by: RaineyMisty <tic128@pitt.edu>

--- src/observer/plugin.rs
+++ src/observer/plugin.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
 use crate::observer::{state::ObservationState, system::update_observation_system};
+use super::system::Observation;
 
 /// The main plugin entry for the observer system.
 /// This plugin spawns and updates the observation state,
@@ -8,7 +9,7 @@ pub struct ObserverPlugin;
 
 impl Plugin for ObserverPlugin {
     fn build(&self, app: &mut App) {
-        app
+        app.add_event::<Observation>()
             .init_resource::<ObservationState>() 
             .add_systems(Update, update_observation_system);
     }
